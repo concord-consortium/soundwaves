89ee8585229ad25da12ee6e8121a0be108220ec7
Concord-Jim <jabbott@concord.org>
2021-10-27 12:11:13 -0400

    Simple work-around for App render test failure (without pulling in another library)

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { App } from "./app";
 
-describe.skip("App", () => {
+describe("App", () => {
   it("shows the header", () => {
     render(<App />);
     expect(screen.getByText("Sounds are waves")).toBeDefined();
