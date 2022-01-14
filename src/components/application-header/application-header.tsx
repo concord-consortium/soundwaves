7f33e0e0b27e9d6bd6ebb722aebee48b655be21c
Concord-Jim <jabbott@concord.org>
2022-01-14 11:22:25 -0500

    Adds shorter logo image; and re-sizes height area height; text font-size correspondingly.

--- src/components/application-header/application-header.tsx
+++ src/components/application-header/application-header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./application-header.scss";
 
-import WavesLogo from "../../assets/wavesLogo.png";
+import WavesLogo from "../../assets/WavesLogo_30pxh.png";
 
 export const AppHeader = () => {
   return (
